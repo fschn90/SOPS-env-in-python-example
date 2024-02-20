3e9fa3502412ef7ba03e5b4a933300f7cfb27821
fschn90 <hello@fschn.org>
2024-02-20 16:31:54 +0100

    sops integration into python

--- test.py
+++ test.py
@@ -1,7 +1,10 @@
-import os
+import os, subprocess
 from dotenv import load_dotenv
+from io import StringIO
 
-load_dotenv(".env")
+decrypted = subprocess.run(['sops', '-d', '.env'], capture_output=True, text=True)
 
+conf=StringIO(decrypted.stdout)
+load_dotenv(stream=conf)
 print(os.environ.get("secret"))
 
