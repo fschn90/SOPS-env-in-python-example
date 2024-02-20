d9c1c41ac99037b67d184fd5b62fb9bdecfde937
fschn90 <hello@fschn.org>
2024-02-20 16:45:49 +0100

    more setup and minor changes

--- test.py
+++ test.py
@@ -3,8 +3,10 @@ from dotenv import load_dotenv
 from io import StringIO
 
 decrypted = subprocess.run(['sops', '-d', '.env'], capture_output=True, text=True)
-
 conf=StringIO(decrypted.stdout)
+
 load_dotenv(stream=conf)
-print(os.environ.get("secret"))
+print('secret: ', os.environ.get("secret"))
+print('user: ', os.environ.get("user"))
+print('password: ', os.environ.get("password"))
 
